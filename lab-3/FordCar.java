11d0dce1d399ce99f2497ca34474b7639e5f396e
xarim96 <mohammadsarim96@gmail.com>
2015-09-05 01:22:54 +0500

    Create FordCar.java

---
+++ lab-3/FordCar.java
@@ -0,0 +1,28 @@
+public class FordCar {
+	
+	public String Color, Type, Model;
+	public int Cylinder;
+	
+	//Start, Accelerate, Reverse, Stop
+	
+	public void Start()
+	{
+		
+	}
+	
+	public void Accelerate()
+	{
+		
+	}
+	
+	public void Reverse()
+	{
+		
+	}
+	
+	public void Stop()
+	{
+		
+	}
+
+}
